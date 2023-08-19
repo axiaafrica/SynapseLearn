313acb60a4ecf63a628399f2932d83dc728e3604
Adedoyinlorukomi <akindoyin800@gmail.com>
2023-08-19 12:26:00 +0100

    Commit to main

---
+++ src/pages/PasswordRecovery.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../assets/style.css'
+
+const PasswordRecovery = () => {
+  return (
+    <div><h1>this page is for <strong>John doe</strong></h1>
+    <p>do not code the navbar and footer. </p></div>
+  )
+}
+
+export default PasswordRecovery
